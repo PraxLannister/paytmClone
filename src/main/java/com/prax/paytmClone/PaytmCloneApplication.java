9dae2a79c693369f028c5112b09c025ed60620ec
Prakhar Shekhar Parthasarthi <prakharsp1502077@gmail.com>
2021-11-02 06:36:20 +0530

    Initial commit Files Generated By Spring Initializer

---
+++ src/main/java/com/prax/paytmClone/PaytmCloneApplication.java
@@ -0,0 +1,13 @@
+package com.prax.paytmClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaytmCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaytmCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prax/paytmClone/PaytmCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prax.paytmClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaytmCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
